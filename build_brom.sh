27f51cbd27850662f1fcbb589f8e4e4088125dfc
Shamil Yakubov <shamilyakubov@hotmail.com>
2016-04-02 12:52:58 +0300

    Add mkdir command

--- build_brom.sh
+++ build_brom.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-rm -r build/* && cp data/* build &&
+mkdir -p build
+rm -r build/*
+cp data/* build &&
 cd build && cmake $BROMDIR/code -DFABM_BASE=$FABMDIR -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=~/local/brom
